37a3021f9efb20249657f940c0cdd70ca39cc949
Amine Abboudi <abboudi.med@gmail.com>
2025-09-29 11:49:36 +0200

    Remove .next files from Git tracking - properly ignore build artifacts

--- frontend/.next/static/chunks/app_page_tsx_aed104ab._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_2c765060._.js",
-  "static/chunks/_6a4fc8ec._.js"
-],
-    source: "dynamic"
-});
